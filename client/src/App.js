d50bc40e3635b823d4ab3952cee4f3d3bb24280b
richie48 <odiorrichard@gmail.com>
2021-10-18 00:03:28 +0100

    forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 
 //I didnt know it is important that you import an image in react before using it.
 import memoryimg from './images/memory.png';
@@ -6,40 +6,40 @@ import memoryimg from './images/memory.png';
 import Form from './component/Form/Form.js';
 import Posts from './component/Posts/Posts.js';
 
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import {getPosts} from './actions/post'
+import { getPosts } from './actions/post'
 
 //installed tailwind > https://www.ordinarycoders.com/blog/article/reactjs-tailwindcss
 
 // import
 
 const App = () => {
-  const  dispatch= useDispatch()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(getPosts())
   }, [dispatch])
 
   return (
-    <div class="container mx-auto max-w-lg">
-      <div class="bg-black p-3 px-10 rounded-sm item-center">
-        <ul class="flex justify-between">
-          <li class="mr-2">
-            <h1 class="text-white hover:text-black text-center text-xl">
+    <div className="container mx-auto max-w-xl">
+      <div className="bg-black p-3 px-10 rounded-sm item-center">
+        <ul className="flex justify-between">
+          <li className="mr-2">
+            <h1 className="text-white hover:text-black text-center text-xl">
               Memories
             </h1>
           </li>
           <li>
             <img
-              class="object-contain w-10 h-10"
+              className="object-contain w-10 h-10"
               src={memoryimg}
               alt="memories"
             />
           </li>
         </ul>
       </div>
-      <div class="grid grid-cols-2 max-w-lg bg-yellow-100">
+      <div className="grid grid-cols-2 bg-yellow-100">
         <div>
           <Posts />
         </div>
--- client/src/component/Form/Form.js
+++ client/src/component/Form/Form.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
 const Form = () => {
+  const handleSubmit = ()=>{
+
+  }
   return (
     <>
-      <h1>FORM</h1>
+      <form className="" autoComplete="off" onSubmit={handleSubmit}>
+        <h6 className="font-bold item-right">Create a memory</h6>
+        <div className=""> 
+        <label className="block text-grey-600 text-sm ">Creator
+        </label>
+        <input className="text-grey-600 shadow bg-red-100" for="creator text-sm" type="text" placeholder="creator"></input>
+        </div>
+
+        <div>
+        <label className="block text-grey-600 text-sm ">Title
+        </label>
+        <input className="text-grey-600 shadow bg-red-100 " for="creator text-sm" type="text" placeholder="title"></input>
+        </div> 
+
+        <div>
+        <label className="block text-grey-600 text-sm ">Title
+        </label>
+        <input className="text-grey-600 shadow bg-red-100 " for="creator text-sm" type="text" placeholder="title"></input>
+        </div> 
+
+
+      </form>
     </>
   );
 };
