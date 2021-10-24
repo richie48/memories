ff66d79349f927c12a09fe58d9c4412fe46d68f9
richie48 <odiorrichard@gmail.com>
2021-10-24 01:26:37 +0100

    dynamically add tags

--- client/src/component/Posts/Post/Post.js
+++ client/src/component/Posts/Post/Post.js
@@ -4,15 +4,13 @@ import React from 'react';
 const Post = ({post,key}) => {
   return (
     <div className="m-1 p-2 max-w-sm rounded bg-blue-50 shadow-lg min-h-md">
-      <img src={post.selectedFile} alt="?? no image"></img>
+      <img src={post.selectedFile} alt="?? no display"></img>
       <div className="px-3 py-4">
         <div className="font-bold text-xl mb-2">{post.title}</div>
         <p className="text-gray-700 text-sm">{post.message}</p>
       </div>
       <div class="px-2 pt-2 pb-1">
-    <span className="bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-1 mb-1">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-1 mb-1">#winter</span>
+        {post.tags.length && post.tags.map((tag)=><span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-1 mb-1">#{tag}</span>)}
   </div>
 
     </div>
